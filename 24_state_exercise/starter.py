b1ac7ca9898a9fe5aa1c2b11b457496ca3cc6c7e
JKjeldsen <inty82@gmail.com>
2025-04-13 10:36:48 +0200

    update progress

--- 24_state_exercise/starter.py
+++ 24_state_exercise/starter.py
@@ -23,23 +23,79 @@ st.subheader("Temperature conversion")
 # Write a callback to sets the temperatures depending on
 # which button the user clicks. Use kwargs.
 
+if "celsius" not in st.session_state:
+    st.session_state["celsius"] = 0.00
+
+if "fahrenheit" not in st.session_state:
+    st.session_state["fahrenheit"] = 32.00
+
+if "kelvin" not in st.session_state:
+    st.session_state["kelvin"] = 273.15
+
+def convert(deg_type: str):
+    cel = st.session_state["celsius"]
+    fah = st.session_state["fahrenheit"]
+    kel = st.session_state["kelvin"]
+
+    if deg_type == "c":
+        st.session_state["fahrenheit"] = (cel * 9/5) + 32
+        st.session_state["kelvin"] = cel + 273.15
+    elif deg_type == "f":
+        st.session_state["celsius"] = (fah - 32) * 5/9
+        st.session_state["kelvin"] = ((fah - 32) * 5/9) + 273.15
+    elif deg_type == "k":
+        st.session_state["celsius"] = kel - 273.15
+        st.session_state["fahrenheit"] = ((kel - 273.15) * 9/5) + 32
+    else:
+        pass
+
+def celsius_add(_num):
+    st.session_state['celsius'] += _num
+    convert('c')
+
+def set_temperatures(celsius, fahrenheit, kelvin):
+    st.session_state['celsius'] = celsius
+    st.session_state['fahrenheit'] = fahrenheit
+    st.session_state['kelvin'] = kelvin
+
 col1, col2, col3 = st.columns(3)
 
 # Hook up the first 3 callbacks to the input widgets
-col1.number_input("Celsius", step=0.01, key="celsius")
-col2.number_input("Farenheit", step=0.01, key="farenheit")
-col3.number_input("Kelvin", step=0.01, key="kelvin")
+col1.number_input("Celsius",
+    step=0.01,
+    key="celsius",
+    on_change=convert,
+    args=('c')
+)
+col2.number_input("Fahrenheit",
+    step=0.01,
+    key="fahrenheit",
+    on_change=convert,
+    args=('f')
+)
+col3.number_input("Kelvin",
+    step=0.01,
+    key="kelvin",
+    on_change=convert,
+    args=("k")
+)
 
 # Hook up the 4th callback to the button. Use args.
 col1, _, _ = st.columns(3)
 num = col1.number_input("Add to Celsius", step=1)
-col1.button("Add", type="primary")
+col1.button("Add", type="primary", on_click=celsius_add, args=(num,))
 
 col1, col2, col3 = st.columns(3)
 
 # Hook up the last callback to each button. Use kwargs.
-col1.button('🧊 Freezing point of water')
-col2.button('🔥 Boiling point of water')
-col3.button('🥶 Absolute zero')
+col1.button('🧊 Freezing point of water', 
+            on_click=set_temperatures, 
+            kwargs=dict(celsius=0.00, fahrenheit=32.00, kelvin=273.15))
+col2.button('🔥 Boiling point of water',
+            on_click=set_temperatures,
+            kwargs=dict(celsius=100.00, fahrenheit=212.00, kelvin=373.15))
+col3.button('🥶 Absolute zero',
+            on_click=set_temperatures,
+            kwargs=dict(celsius=-273.15, fahrenheit=-459.67, kelvin=0.00))
 
 st.write(st.session_state)
--- 25_state_capstone/starter.py
+++ 25_state_capstone/starter.py
@@ -3,17 +3,38 @@ import numpy as np
 from joblib import load
 
 # Initialize the session state with an empty prediction
+if "pred" not in st.session_state:
+    st.session_state["pred"] = None
 
 # Function to load the model (use caching)
+@st.cache_resource(show_spinner="Loading model...")
+def load_model():
+    pipe = load('model/model.joblib')
+    return pipe
 
 # Callback function to make a prediction.
 # Use kwargs to pass the model as an argument
 # It updates the value of the prediction stored in the state
+def make_prediction(pipe):
+    miles = st.session_state["miles"]
+    year = st.session_state["year"]
+    make = st.session_state["make"]
+    model = st.session_state["model"]
+    engine_size = st.session_state["engine_size"]
+    province = st.session_state["province"]
+
+    X_pred = np.array([miles, year, make, model, engine_size, province]).reshape(1, -1)
+
+    pred = pipe.predict(X_pred)
+    pred = round(pred[0], 2)
+
+    st.session_state["pred"] = pred
 
 if __name__ == "__main__":
     st.title("🍁Used car price calculator")
 
     # Load model
+    pipe = load_model()
 
     with st.form(key="form"):
         col1, col2, col3 = st.columns(3)
@@ -36,10 +57,14 @@ if __name__ == "__main__":
             st.selectbox("Make", key="make", index=0, options=['toyota', 'honda'])
             st.selectbox("Province", index=0, key="province", options=['NB', 'QC', 'BC', 'ON', 'AB', 'MB', 'SK', 'NS', 'PE', 'NL', 'YT', 'NC', 'OH','SC'])
         
-        st.form_submit_button("Calculate", type="primary")
+        st.form_submit_button("Calculate", type="primary", on_click=make_prediction, kwargs=dict(pipe=pipe))
 
     # Display the prediction
     # If the value is empty, display a message to click on the button
     # Otherwise, display the prediction
-    
+    if st.session_state["pred"] is not None:
+        st.subheader(f"Estimated car price: {st.session_state['pred']}")
+    else:
+        st.write("Input data and calculate")
+
     st.write(st.session_state)
